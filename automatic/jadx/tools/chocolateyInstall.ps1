09161f4abd926ec1e1ff7a95f79982c8ae1e6425
Chocolatey <chocolatey@realdimensions.net>
2022-02-21 03:19:41 +0000

    AU: 1 updated - jadx [skip ci] https://gist.github.com/7d4536b29dacffbc807aaa67914180c6/88b1e5b0dc21339db7011e676f93f69fe8226b7d

--- automatic/jadx/tools/chocolateyInstall.ps1
+++ automatic/jadx/tools/chocolateyInstall.ps1
@@ -17,7 +17,7 @@ if (! (Select-String -Path $tempFile -Pattern 64-Bit)) {
 }
 Remove-Item -Path $tempFile
 
-$filename = "jadx-1.3.2.zip"
+$filename = "jadx-1.3.3.zip"
 $packageArgs = @{
     packageName = $env:ChocolateyPackageName
     destination = $toolsDir
